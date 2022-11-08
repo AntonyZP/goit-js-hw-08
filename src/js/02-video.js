67b00f28a39f179fb70a5328c09beec75a119591
AntonyZP <kowevoyanton@gmail.com>
2022-11-09 01:47:06 +0200

    finish

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,24 +1,16 @@
 import Player from '@vimeo/player';
-console.log(Player);
+import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-// player.on('play', function () {
-//   console.log('played the video!');
-// });
-
-// player.getVideoTitle().then(function (title) {
-//   console.log('title:', title);
-// });
 const onPlay = function (data) {
-  console.log('hello');
   localStorage.setItem(
     'videoplayer-current-time',
     JSON.stringify(data.seconds)
   );
 };
 
-player.on('timeupdate', onPlay);
+player.on('timeupdate', throttle(onPlay, 1000));
 player
   .setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time')))
   .then(function (seconds) {
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
+const STORAGE_KEY = 'feedback-form-state';
+const refs = {
+  feedbackForm: document.querySelector('.feedback-form'),
+};
 
+const formData = {};
+refs.feedbackForm.addEventListener('submit', onFormSubmit);
+refs.feedbackForm.addEventListener('input', throttle(onFormInput, 500));
+populateInput();
+
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+  console.log(formData);
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  if (localStorage.getItem(STORAGE_KEY)) {
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  }
+  e.currentTarget.reset();
+
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function populateInput() {
+  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  //   if (!savedMessage) {
+  //     return;
+  //   }
+  if (savedMessage) {
+    // refs.textarea.value = savedMessage;
+    //   refs.email.value = savedMesage;
+    refs.feedbackForm.email.value = savedMessage.email || '';
+    refs.feedbackForm.message.value = savedMessage.message || '';
+  }
+}
