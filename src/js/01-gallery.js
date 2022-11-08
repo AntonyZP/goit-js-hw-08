f3261cfb8146132d67bf5dd014837af88092d652
AntonyZP <kowevoyanton@gmail.com>
2022-11-08 22:06:31 +0200

    task 2.7

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,24 @@
 // Add imports above this line
 import SimpleLightbox from 'simplelightbox';
-import { galleryItems } from './gallery-items';
-// Change code below this line
-
 console.log(galleryItems);
 console.log(SimpleLightbox);
+import { galleryItems } from './gallery-items.js';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// Change code below this line
+
+const galleryList = document.querySelector('.gallery');
+const markup = galleryItems
+  .map(
+    ({ description, original, preview }) =>
+      `<li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>`
+  )
+  .join('');
+galleryList.insertAdjacentHTML('beforeend', markup);
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: '250',
+});
+console.log(galleryList);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,37 @@
 import Player from '@vimeo/player';
 console.log(Player);
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
+const onPlay = function (data) {
+  console.log('hello');
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
+};
+
+player.on('timeupdate', onPlay);
+player
+  .setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time')))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
